55ac685e34b934b9f500da908c17ad1a22f4a43d
tonycheng93 <bc1341286335@gmail.com>
2015-09-11 19:42:13 +0800

    为RadiButton添加数据源

--- app/src/main/java/com/tony/horizontalscrollview/MainActivity.java
+++ app/src/main/java/com/tony/horizontalscrollview/MainActivity.java
@@ -3,7 +3,10 @@ package com.tony.horizontalscrollview;
 import android.app.Activity;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -13,6 +16,7 @@ public class MainActivity extends Activity {
 
     private InputStream inputStream;
     List<String> xml = new ArrayList<>();
+    private RadioGroup mRadioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +25,7 @@ public class MainActivity extends Activity {
     }
 
     private void initWindow() {
+        mRadioGroup = (RadioGroup) findViewById(R.id.radiogroup);
         inputStream = getClass().getResourceAsStream("/assets/ED_SoftwareShortcuts.xml");
         try {
             xml = XmlParse.getXML(inputStream);
@@ -37,7 +42,17 @@ public class MainActivity extends Activity {
             mRadio.setTextSize(getResources().getDimension(R.dimen.text_size_big_plus));
             mRadio.setButtonDrawable(R.color.transparent);
             mRadio.setTextSize(20);
+            @SuppressWarnings("ResourceType")
             ColorStateList colors = getResources().getColorStateList(R.drawable.selector_keyshead_tab);
+            mRadio.setTextColor(colors);
+            RadioGroup.LayoutParams mRadioParams = new RadioGroup.LayoutParams(RadioGroup.LayoutParams.WRAP_CONTENT,
+                    RadioGroup.LayoutParams.WRAP_CONTENT);
+            mRadioParams.setMargins(22, 0, 22, 0);
+            mRadioGroup.addView(mRadio, mRadioParams);
+            TextView mTextView = new TextView(this);
+            mTextView.setBackgroundColor(getResources().getColor(R.color.key_line));
+            LinearLayout.LayoutParams mLayoutParams = new LinearLayout.LayoutParams(1, RadioGroup.LayoutParams.WRAP_CONTENT);
+            mRadioGroup.addView(mTextView, mLayoutParams);
         }
     }
 }
